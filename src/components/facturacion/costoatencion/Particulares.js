0cac08fa368548d2504e3f54331ffadb0554eb5f
GuidoTorres <hectortorresdurand@gmail.com>
2021-07-09 08:08:11 -0500

    correciones facturacion

--- src/components/facturacion/costoatencion/Particulares.js
+++ src/components/facturacion/costoatencion/Particulares.js
@@ -1,109 +1,109 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import DataTable from 'react-data-table-component';
-import { fparticular } from '../../../data/FParticular';
+import DataTable from "react-data-table-component";
+import { fparticular } from "../../../data/FParticular";
 
-import { paginacionOpciones } from '../../../helpers/tablaOpciones';
-import MParticulares from './MParticulares';
+import { paginacionOpciones } from "../../../helpers/tablaOpciones";
+import MParticulares from "./MParticulares";
 
 const Particulares = () => {
-  const [busqueda, setBusqueda] = useState('');
+  const [busqueda, setBusqueda] = useState("");
   const [listRegistro, setListRegistro] = useState([]);
   const [openModal, setOpenModal] = useState(false);
 
   const columnas = [
     {
-      name: 'Item',
-      selector: 'id',
+      name: "Item",
+      selector: "id",
       sortable: true,
       style: {
-        borderBotton: 'none',
-        color: '#555555',
+        borderBotton: "none",
+        color: "#555555",
       },
     },
     {
-      name: 'Nombre',
-      selector: 'nombre',
+      name: "Nombre",
+      selector: "nombre",
       sortable: true,
       style: {
-        borderBotton: 'none',
-        color: '#555555',
+        borderBotton: "none",
+        color: "#555555",
       },
     },
     {
-      name: 'Apellido',
-      selector: 'apellido',
+      name: "Apellido",
+      selector: "apellido",
       sortable: true,
       style: {
-        borderBotton: 'none',
-        color: '#555555',
+        borderBotton: "none",
+        color: "#555555",
       },
     },
     {
-      name: 'DNI',
-      selector: 'dni',
+      name: "DNI",
+      selector: "dni",
       sortable: true,
       style: {
-        borderBotton: 'none',
-        color: '#555555',
+        borderBotton: "none",
+        color: "#555555",
       },
     },
     {
-      name: 'Fecha',
-      selector: 'fecha',
+      name: "Fecha",
+      selector: "fecha",
       sortable: true,
       style: {
-        borderBotton: 'none',
-        color: '#555555',
+        borderBotton: "none",
+        color: "#555555",
       },
     },
     {
-      name: 'Tipo de servicio',
-      selector: 'tipo',
+      name: "Tipo de servicio",
+      selector: "tipo",
       sortable: true,
       style: {
-        borderBotton: 'none',
-        color: '#555555',
+        borderBotton: "none",
+        color: "#555555",
       },
     },
     {
-      name: 'Plan de atención',
-      selector: 'atencion',
+      name: "Plan de atención",
+      selector: "atencion",
       sortable: true,
       style: {
-        borderBotton: 'none',
-        color: '#555555',
+        borderBotton: "none",
+        color: "#555555",
       },
     },
     {
-      name: 'SubTotal',
-      selector: 'subtotal',
+      name: "SubTotal",
+      selector: "subtotal",
       sortable: true,
       style: {
-        borderBotton: 'none',
-        color: '#555555',
+        borderBotton: "none",
+        color: "#555555",
       },
     },
     {
-      name: 'Impuesto',
-      selector: 'impuesto',
+      name: "Impuesto",
+      selector: "impuesto",
       sortable: true,
       style: {
-        borderBotton: 'none',
-        color: '#555555',
+        borderBotton: "none",
+        color: "#555555",
       },
     },
     {
-      name: 'Total',
-      selector: 'total',
+      name: "Total",
+      selector: "total",
       sortable: true,
       style: {
-        borderBotton: 'none',
-        color: '#555555',
+        borderBotton: "none",
+        color: "#555555",
       },
     },
     {
-      name: 'Liquidar',
+      name: "Liquidar",
       button: true,
       cell: (e) => (
         <button
@@ -121,29 +121,29 @@ const Particulares = () => {
       const search = fparticular.filter((data) => {
         return (
           data.nombre
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, '')
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
             .toLocaleLowerCase()
             .includes(busqueda) ||
           data.apellido
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, '')
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
             .toLocaleLowerCase()
             .includes(busqueda) ||
           data.dni.toString().includes(busqueda) ||
           data.fecha
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, '')
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
             .toLocaleLowerCase()
             .includes(busqueda) ||
           data.tipo
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, '')
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
             .toLocaleLowerCase()
             .includes(busqueda) ||
           data.atencion
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, '')
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
             .toLocaleLowerCase()
             .includes(busqueda) ||
           data.subtotal.toString().includes(busqueda) ||
@@ -180,16 +180,74 @@ const Particulares = () => {
             </div>
           </div>
 
-          <DataTable
-            columns={columnas}
-            data={listRegistro}
-            pagination
-            paginationComponentOptions={paginacionOpciones}
-            fixedHeader
-            fixedHeaderScrollHeight="500px"
-            noDataComponent={<i className="fas fa-inbox table__icono"></i>}
-            selectableRows
-          />
+          <div class="accordion mt-4" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingOne">
+                <button
+                  class="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseOne"
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  Particular
+                </button>
+              </h2>
+              <div
+                id="collapseOne"
+                class="accordion-collapse collapse show"
+                aria-labelledby="headingOne"
+                data-bs-parent="#accordionExample"
+              >
+                <DataTable
+                  columns={columnas}
+                  data={listRegistro}
+                  pagination
+                  paginationComponentOptions={paginacionOpciones}
+                  fixedHeader
+                  fixedHeaderScrollHeight="500px"
+                  noDataComponent={
+                    <i className="fas fa-inbox table__icono"></i>
+                  }
+                  selectableRows
+                />
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingTwo">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="collapseTwo"
+                >
+                  Empresa
+                </button>
+              </h2>
+              <div
+                id="collapseTwo"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingTwo"
+                data-bs-parent="#accordionExample"
+              >
+                <DataTable
+                  columns={columnas}
+                  data={listRegistro}
+                  pagination
+                  paginationComponentOptions={paginacionOpciones}
+                  fixedHeader
+                  fixedHeaderScrollHeight="500px"
+                  noDataComponent={
+                    <i className="fas fa-inbox table__icono"></i>
+                  }
+                  selectableRows
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       {openModal && (
