a912fc84b4487794d7db1b644dc826ba4c4c9780
Maverick <maverick.champi@gmail.com>
2021-07-12 12:24:56 -0500

    diseño menu, modal usuario, label en tables

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Clinica from './Clinica';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Clinica from "./Clinica";
+import reportWebVitals from "./reportWebVitals";
 
-import './scss/style.scss';
+import "./scss/style.scss";
 // console.log(process.env);
-ReactDOM.render(<Clinica />, document.getElementById('root'));
+ReactDOM.render(<Clinica />, document.getElementById("root"));
 
 reportWebVitals();
