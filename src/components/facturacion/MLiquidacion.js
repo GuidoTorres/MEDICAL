fcf044e9a82698946150d3f67e1b05b250ba2448
Maverick <maverick.champi@gmail.com>
2021-07-27 15:12:51 -0500

    facturacion

--- src/components/facturacion/MLiquidacion.js
+++ src/components/facturacion/MLiquidacion.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Modal from "react-modal";
 import { customStyles } from "../../helpers/tablaOpciones";
 import { fetchGETPOSTPUTDELETEJSON } from "../../helpers/fetch";
 
-const MLiquidacion = ({ openModal, setOpenModal, datos }) => {
+const MLiquidacion = ({
+  openModal,
+  setOpenModal,
+  datos,
+  getLiquidacion,
+  setBusqueda,
+}) => {
+  useEffect(() => {
+    setBusqueda(null);
+  }, []);
+
   const closeModal = () => {
     setOpenModal(false);
   };
 
-  console.log(datos);
+  // console.log(datos);
 
   const deleteLiquidacion = () => {
     fetchGETPOSTPUTDELETEJSON(`settlement/${datos.id}`, {}, "DELETE")
       .then((info) => info.json())
-      .then((info) => console.log(info));
+      .then(() => {
+        getLiquidacion();
+      });
   };
 
   const handleEliminar = () => {
     deleteLiquidacion();
+    closeModal();
+    // setBusqueda("");
   };
 
   const openPdf = () => {
@@ -69,7 +83,7 @@ const MLiquidacion = ({ openModal, setOpenModal, datos }) => {
       <div className="col-12 fmparticular">
         <div>
           <div className="table-responsive">
-            <table class="table">
+            <table className="table">
               <thead>
                 <tr>
                   <th scope="col">Item</th>
@@ -91,8 +105,8 @@ const MLiquidacion = ({ openModal, setOpenModal, datos }) => {
                         <td>{data.attention.date_attention}</td>
                         <td>{data.attention.person.name}</td>
                         <td>{data.attention.person.pat_lastname}</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
+                        <td>{data.attention.service.name}</td>
+                        <td>{data.attention.service.description}</td>
                         <td>{data.attention.amount}</td>
                       </tr>
                     ))
@@ -103,7 +117,7 @@ const MLiquidacion = ({ openModal, setOpenModal, datos }) => {
                   <td>{datos.subtotal ? datos.subtotal : ""}</td>
                 </tr>
                 <tr>
-                  <td colspan="7">
+                  <td colSpan="7">
                     {" "}
                     <textarea
                       defaultValue={datos.observation ? datos.observation : ""}
@@ -114,7 +128,7 @@ const MLiquidacion = ({ openModal, setOpenModal, datos }) => {
                   <td>{datos.igv ? datos.igv : ""}</td>
                 </tr>
                 <tr>
-                  <td colspan="7"></td>
+                  <td colSpan="7"></td>
                   <td>Total</td>
                   <td>{datos.amount ? datos.amount : ""}</td>
                 </tr>
@@ -137,10 +151,10 @@ const MLiquidacion = ({ openModal, setOpenModal, datos }) => {
         </div>
         <div className="group-2">
           <button className="botones me-1" onClick={closeModal}>
-            Retroceder
+            Cancelar
           </button>
-          <button className="botones" onClick={handleEliminar}>
-            Dar de baja
+          <button className="btn btn-danger" onClick={handleEliminar}>
+            Eliminar
           </button>
         </div>
       </div>
