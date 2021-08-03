16e55e3a22a41f9ca382f668acf423f3fdc616be
Maverick <maverick.champi@gmail.com>
2021-08-03 17:10:32 -0500

    editar precios facturacion

--- src/components/facturacion/costoatencion/MMEmpresa.js
+++ src/components/facturacion/costoatencion/MMEmpresa.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 import Swal from "sweetalert2";
 import { customStyles } from "../../../helpers/tablaOpciones";
-// import MMAParticulares from "./MMAParticulares";
+import MPrecio from "./MPrecio";
 import { fetchGETPOSTPUTDELETEJSON } from "../../../helpers/fetch";
 
 const MMEmpresa = ({
@@ -21,26 +21,36 @@ const MMEmpresa = ({
     };
   }, []);
 
-  //   const [openModalCrear, setOpenModalCrear] = useState(false);
+  const [openModalPrecio, setOpenModalPrecio] = useState(false);
   const [subtotal, setSubTotal] = useState(0);
   const [igv, setIgv] = useState(0);
   const [total, setTotal] = useState(0);
-  const [incremento, setIncremento] = useState(0);
   const [fechaActual, setFechaActual] = useState("");
   const [observacion, setObservacion] = useState("");
   const [codigo, setCodigo] = useState("");
+  const [estado, setEstado] = useState(2); // 1 - pediente, 2 - aprobado
+  const [services, setServices] = useState([]);
 
-  // const handleAgregarCliente = () => {
-  //   setOpenModalCrear(true);
-  // };
+  const obtenServicios = () => {
+    const array = data.map((d) => ({ servicio: d.servicio, total: d.total }));
 
-  const editar = () => {
-    document.getElementById("liquidacion-total").readOnly = false;
+    let serviceMap = array.map((item) => {
+      return [item.servicio, item];
+    });
+    var serviceMapArr = new Map(serviceMap); // Pares de clave y valor
+
+    setServices([...serviceMapArr.values()]); // Conversión a un array
   };
 
-  const restaurar = () => {
-    document.getElementById("liquidacion-total").readOnly = true;
-    document.getElementById("liquidacion-total").value = total;
+  const calcularSubtotal = (servicio) => {
+    const { total } = services.filter((s) => s.servicio === servicio)[0];
+    const subtotal = total * 0.82;
+    return subtotal.toFixed(2);
+  };
+
+  const editar = () => {
+    setOpenModalPrecio(true);
+    // console.log(services);
   };
 
   const closeModal = () => {
@@ -88,8 +98,9 @@ const MMEmpresa = ({
       const liquidacion = {
         code: codigo,
         observation: observacion,
+        isapproved: estado,
         subtotal: subtotal,
-        amount: total + incremento,
+        amount: total,
         igv: igv,
         attentions: array,
         company_id: dataEmpresa.id,
@@ -106,16 +117,37 @@ const MMEmpresa = ({
       });
     }
   };
+  const calcularValores = () => {
+    let suma = 0;
+    let igv = 0;
+    let totalPrecio = 0;
+    data.map((d) => {
+      if (estado === 2) {
+        suma += Number(d.subtotal);
+        igv += Number(d.igv);
+        totalPrecio += Number(d.total);
+      } else {
+        const { total } = services.filter((s) => s.servicio === d.servicio)[0];
+        const sub = (total * 0.82).toFixed(2);
+        const calcIgv = (total * 0.18).toFixed(2);
+
+        suma += Number(sub);
+        igv += Number(calcIgv);
+        totalPrecio += total;
+      }
+    });
+    setSubTotal(Number(suma.toFixed(2)));
+    setIgv(Number(igv.toFixed(2)));
+    setTotal(Number(totalPrecio.toFixed(2)));
+  };
 
   useEffect(() => {
-    let suma = 0;
-    data.map((d) => {
-      suma += d.total;
-    });
-    setSubTotal(suma);
-    setIgv(Math.round(suma * 0.18));
-    setTotal(Math.round(suma * 1.18));
+    calcularValores();
     enviarfechaActual();
+  }, [subtotal, igv, total, estado]);
+
+  useEffect(() => {
+    obtenServicios();
   }, []);
 
   return (
@@ -175,7 +207,7 @@ const MMEmpresa = ({
                     <th scope="col">Nombres y apellidos</th>
                     <th scope="col">Tipo de prueba</th>
                     <th scope="col">Servicio</th>
-                    <th scope="col">Costo</th>
+                    <th scope="col">Sub-total</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -187,7 +219,11 @@ const MMEmpresa = ({
                       <td>{d.paciente}</td>
                       <td>{d.categoria}</td>
                       <td>{d.servicio}</td>
-                      <td>{d.total}</td>
+                      <td>
+                        {estado === 1
+                          ? calcularSubtotal(d.servicio)
+                          : d.subtotal}
+                      </td>
                     </tr>
                   ))}
 
@@ -221,14 +257,7 @@ const MMEmpresa = ({
                       <strong>Total</strong>
                     </td>
                     <td>
-                      <input
-                        id="liquidacion-total"
-                        type="number"
-                        defaultValue={total + incremento}
-                        onChange={(e) => setIncremento(e.target.value - total)}
-                        readOnly
-                        min="0"
-                      />
+                      <strong>{total}</strong>
                     </td>
                   </tr>
                 </tbody>
@@ -237,10 +266,7 @@ const MMEmpresa = ({
           </div>
         </div>
         <button className="liquidacion-icon" onClick={editar}>
-          <i className="fas fa-pen-square"></i> Edital total
-        </button>
-        <button className="liquidacion-icon" onClick={restaurar}>
-          <i className="fas fa-redo-alt"></i>Restaurar total
+          <i className="fas fa-pen-square"></i> Editar precio(s)
         </button>
 
         {/* {openModalCrear && (
@@ -258,6 +284,18 @@ const MMEmpresa = ({
           </button>
         </div>
       </div>
+      {openModalPrecio && (
+        <MPrecio
+          openModalPrecio={openModalPrecio}
+          setOpenModalPrecio={setOpenModalPrecio}
+          data={data}
+          setEstado={setEstado}
+          services={services}
+          setServices={setServices}
+          obtenServicios={obtenServicios}
+          calcularValores={calcularValores}
+        />
+      )}
     </Modal>
   );
 };
